c18df87aa8fd6e5cc6df73ccad626e246255835f
Tawfik Ahmed <tawfikahmed1x@gmail.com>
2025-10-13 22:53:42 +0300

    finish auth logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+
 const App = () => {
-  return <div>Hi</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route
+        path="/sso-callback"
+        element={
+          <AuthenticateWithRedirectCallback
+            signUpForceRedirectUrl={"/auth-callback"}
+          />
+        }
+      />
+      <Route path="/auth-callback" element={<AuthCallbackPage />} />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/components/SignInOAuthButtons.tsx
@@ -0,0 +1,31 @@
+import { useSignIn } from "@clerk/clerk-react";
+import { Button } from "./ui/button";
+
+const SignInOAuthButtons = () => {
+  const { signIn, isLoaded } = useSignIn();
+
+  if (!isLoaded) {
+    return null;
+  }
+
+  const handleSignIn = () => {
+    signIn.authenticateWithRedirect({
+      strategy: "oauth_google",
+      redirectUrl: "/sso-callback",
+      redirectUrlComplete: "/auth-callback",
+    });
+  };
+
+  return (
+    <Button
+      onClick={handleSignIn}
+      variant={"secondary"}
+      className="text-white border-zinc-200 h-11"
+    >
+      Continue with Google
+    </Button>
+  );
+  return <div></div>;
+};
+
+export default SignInOAuthButtons;
